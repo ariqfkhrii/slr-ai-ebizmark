c418aff5fba16bd6bdfa5fde97385a375b22799e
ariqfkhrii <ariqfkhrii@gmail.com>
2026-05-23 10:11:19 +0700

    #7 | @ariqfkhrii | feat: Implement ResearchPlanKeyword management with controller, requests, and service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\ResearchPlanKeyword\ResearchPlanKeywordService;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
@@ -15,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ResearchPlanKeywordService::class, function ($app) {
+            return new ResearchPlanKeywordService();
+        });
     }
 
     /**

af66657442347d87c4d4877b2493111195379b56
Firlyansyah <undeadcatgirl5@gmail.com>
2026-06-17 20:48:30 +0700

    @Firlyansyah | feat: Setup CI/CD

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,4 +9,7 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+echo "===== Laravel Log ====="
+cat storage/logs/laravel.log || true
+
 exec /usr/bin/supervisord -c /etc/supervisord.conf

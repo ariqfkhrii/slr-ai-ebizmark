3c54a082f31a4e969e593a89b5f814c22eaff3af
Firlyansyah <undeadcatgirl5@gmail.com>
2026-06-17 20:15:24 +0700

    @Firlyansyah | feat: Setup CI/CD

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,49 +1,12 @@
 #!/bin/sh
 set -e
 
-echo "Starting MariaDB..."
+php artisan config:clear
 
-# Init database jika belum ada
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing MariaDB..."
-    mariadb-install-db \
-        --user=mysql \
-        --datadir=/var/lib/mysql
-fi
+php artisan migrate --force
 
-# Socket directory
-mkdir -p /run/mysqld
-chown mysql:mysql /run/mysqld
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
-# Start MariaDB
-mariadbd \
-  --user=mysql \
-  --datadir=/var/lib/mysql \
-  --socket=/run/mysqld/mysqld.sock &
-
-# Wait until ready
-until mariadb-admin ping --socket=/run/mysqld/mysqld.sock --silent; do
-    echo "Waiting for MariaDB..."
-    sleep 2
-done
-
-echo "Creating database..."
-
-mariadb --socket=/run/mysqld/mysqld.sock -u root <<EOF
-CREATE DATABASE IF NOT EXISTS demo_db;
-CREATE USER IF NOT EXISTS 'demo_user'@'localhost' IDENTIFIED BY 'demo_password';
-GRANT ALL PRIVILEGES ON demo_db.* TO 'demo_user'@'localhost';
-FLUSH PRIVILEGES;
-EOF
-
-echo "Running migrations..."
-
-php artisan migrate --force || true
-
-php artisan config:cache || true
-php artisan route:cache || true
-php artisan view:cache || true
-
-echo "Starting nginx/php-fpm..."
-
-exec /init
+exec /usr/bin/supervisord -c /etc/supervisord.conf

643f56608760639db602ff134d841ed946fe5421
IlhamFaisalR23 <ilhamfaisalrr@gmail.com>
2026-07-02 13:42:09 +0700

    #96 | @IlhamFaisalR23 | fix: auth screen style

--- resources/js/pages/auth/reset-password.tsx
+++ resources/js/pages/auth/reset-password.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
+import AuthLayout from '@/layouts/auth-layout';
 import { update } from '@/routes/password';
 
 type Props = {
@@ -14,7 +15,10 @@ type Props = {
 
 export default function ResetPassword({ token, email }: Props) {
     return (
-        <>
+        <AuthLayout
+            title="Reset password"
+            description="Please enter your new password below"
+        >
             <Head title="Reset password" />
 
             <Form
@@ -83,11 +87,6 @@ export default function ResetPassword({ token, email }: Props) {
                     </div>
                 )}
             </Form>
-        </>
+        </AuthLayout>
     );
 }
-
-ResetPassword.layout = {
-    title: 'Reset password',
-    description: 'Please enter your new password below',
-};
